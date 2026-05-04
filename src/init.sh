ce578e64f4b30b49a0e5ff8032daca90076808d1
ParkSnoopy <ParkSnoopy@users.noreply.github.com>
2026-05-05 02:23:58 +0800

    update: default ubuntu archive server as South Korea

--- src/init.sh
+++ src/init.sh
@@ -1,10 +1,12 @@
 #!/bin/env bash
 
 sudo apt update
-sudo apt upgrade -y
 sudo apt install -y ca-certificates apt-transport-https
 sudo sed -i 's|http://|https://|g' /etc/apt/sources.list.d/*
+sudo sed -i 's|https://archive.ubuntu.com/ubuntu|https://kr.archive.ubuntu.com/ubuntu|g'  /etc/apt/sources.list.d/*
+sudo sed -i 's|https://security.ubuntu.com/ubuntu|https://kr.archive.ubuntu.com/ubuntu|g' /etc/apt/sources.list.d/*
 sudo apt update
+sudo apt upgrade -y
 sudo apt install -y curl wget nano zip unzip git python3 python-is-python3 python3-pip
 python -m pip install --break-system-packages uv ruff
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
