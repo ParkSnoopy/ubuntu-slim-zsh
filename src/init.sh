5e8209ef5ff3979057811565eb72bcc64713b91e
ParkSnoopy <ParkSnoopy@users.noreply.github.com>
2026-05-14 19:18:34 +0800

    add(init.sh): add "nodejs with pnpm"

--- src/init.sh
+++ src/init.sh
@@ -1,21 +1,38 @@
 #!/bin/env bash
 
+# change repo: "kr.archive.ubuntu.com/ubuntu"
 sudo sed -i  's|http://archive.ubuntu.com/ubuntu|http://kr.archive.ubuntu.com/ubuntu|g' /etc/apt/sources.list.d/*
 sudo sed -i 's|http://security.ubuntu.com/ubuntu|http://kr.archive.ubuntu.com/ubuntu|g' /etc/apt/sources.list.d/*
+
+# http -> https
 sudo apt update
 sudo apt install -y ca-certificates apt-transport-https
 sudo sed -i 's|http://|https://|g' /etc/apt/sources.list.d/*
+
+# basic tools
 sudo apt update
 sudo apt upgrade -y
 sudo apt install -y curl wget nano zip unzip git python3 python-is-python3 python3-pip
 python -m pip install --break-system-packages uv ruff
+
+# oh-my-zsh with daveverwer theme
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 sed -i 's/^ZSH_THEME="robbyrussell"/ZSH_THEME="daveverwer"/g' $HOME/.zshrc
+
+# Bun
 curl -fsSL https://bun.sh/install | bash
 
+# NodeJS
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.4/install.sh | bash
+\. "$HOME/.nvm/nvm.sh"
+nvm install 22
+yes | corepack enable pnpm
+
+# Post comment
 echo
 echo "--------------------"
 echo "Restart container to take effect"
 echo "--------------------"
 
+# remove self
 rm $0
